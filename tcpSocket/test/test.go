7baf556375d5508cab598dc82dbf4b8b106fe094
yero0214 <yero0214@gmail.com>
2021-08-19 14:39:01 +0900

    commit

--- tcpSocket/test/test.go
+++ tcpSocket/test/test.go
@@ -2,16 +2,40 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"log"
+	"net"
 )
 
 func main() {
-	// var slice []byte
-	// var buffer [4096]byte
-	// slice[1] = buffer[100]
-	// fmt.Printf("%s", slice[0:100])
-	var test string
-	test = "01"
-	num, _ := strconv.Atoi(test)
-	fmt.Println(num)
+
+	l, err := net.Listen("tcp", ":9393")
+	if nil != err {
+		log.Println(err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if nil != err {
+			log.Println(err)
+			continue
+		}
+
+		go ConnHandler(conn)
+	}
+}
+
+func ConnHandler(conn net.Conn) {
+	recvBuf := make([]byte, 4096)
+	for {
+		n, err := conn.Read(recvBuf)
+		if nil != err {
+			log.Println(err)
+		}
+		if 0 < n {
+			data := recvBuf[:n]
+			fmt.Println(string(data[:n]))
+			conn.Write(data)
+		}
+	}
 }
