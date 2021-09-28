237e2eccd41d19169d3a9737dd31f40c98f641dc
yero0214 <yero0214@gmail.com>
2021-09-28 18:18:01 +0900

    commit

--- tcpSocket/server2/goroutine.go
+++ tcpSocket/server2/goroutine.go
@@ -15,16 +15,18 @@ func inGame() {
 		typeOfService := make([]byte, 4)
 		payloadLength := make([]byte, 4)
 		for _, v := range users {
-			userId := make([]byte, 4)
-			x := make([]byte, 4)
-			y := make([]byte, 4)
-			math.Float32bits(v.x)
-			binary.LittleEndian.PutUint32(userId, v.userId)
-			binary.LittleEndian.PutUint32(x, math.Float32bits(v.x))
-			binary.LittleEndian.PutUint32(y, math.Float32bits(v.y))
-			payload = append(payload, userId...)
-			payload = append(payload, x...)
-			payload = append(payload, y...)
+			if v.x != v.cx || v.y != v.cy {
+				userId := make([]byte, 4)
+				x := make([]byte, 4)
+				y := make([]byte, 4)
+				math.Float32bits(v.x)
+				binary.LittleEndian.PutUint32(userId, v.userId)
+				binary.LittleEndian.PutUint32(x, math.Float32bits(v.x))
+				binary.LittleEndian.PutUint32(y, math.Float32bits(v.y))
+				payload = append(payload, userId...)
+				payload = append(payload, x...)
+				payload = append(payload, y...)
+			}
 		}
 		binary.LittleEndian.PutUint32(typeOfService, 0)
 		binary.LittleEndian.PutUint32(payloadLength, uint32(len(payload)))
@@ -32,8 +34,9 @@ func inGame() {
 		packetHeader := append(typeOfService, payloadLength...)
 		buffer = append(buffer, packetHeader...)
 		buffer = append(buffer, payload...)
-
-		broadCast(buffer)
+		if len(payload) != 0 {
+			broadCast(buffer)
+		}
 	}
 }
 
